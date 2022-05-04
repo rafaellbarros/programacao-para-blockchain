5604e0799fe85e24c23850f141566052ca8a7e9d
Rafael <rafaelbarros.df@gmail.com>
2022-05-03 21:29:56 -0300

    :construction: Funcoes Aninhadas

--- 03-cap/functions/js/functions.js
+++ 03-cap/functions/js/functions.js
@@ -28,3 +28,28 @@ const resultado2 = document.getElementById("resultado2");
 resultado2.innerHTML = `<h1>Exemplo 02</h1><p>${func1Comissao}, ${func2Comissao}, ${func3Comissao}</p>`;
 console.log('Exemplo 1');
 console.log(func1Comissao, func2Comissao, func3Comissao);
+
+// Exemplo 3
+function calculaComissaoTurno(salario, turno) {
+    const comissao = calculaComissao(salario);
+    let msg = '';
+    if (turno == 'Noite') {
+        msg = `Turno ${turno} recebe adicional de 100 reais. Comissao = ${(comissao + 100)}`;
+        console.log(msg)
+    } else {
+        msg = `Turno ${turno} recebe valor normal. Comissao = ${comissao}`;
+        console.log(msg)
+    }
+    return msg;
+}
+
+console.log('Exemplo 3')
+const co1 = calculaComissaoTurno(1000, 'Noite');
+const co2 = calculaComissaoTurno(6500, 'Dia');
+const co3 = calculaComissaoTurno(8000, 'Noite');
+
+const resultado3 = document.getElementById("resultado3");
+resultado3.innerHTML = `<h1>Exemplo 03</h1><p>${co1}</p><p>${co2}</p><p>${co3}</p>`;
+
+
+
